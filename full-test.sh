ee079ac9fcc0464a54df79b6c78b57ea0626c403
squli <feokt.ao@gmail.com>
2023-09-16 01:26:29 +0300

    assignment-3-part-2

--- full-test.sh
+++ full-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script can be copied into your base directory for use with
+# This script can be copied into your base directory for use  with
 # automated testing using assignment-autotest.  It automates the
 # steps described in https://github.com/cu-ecen-5013/assignment-autotest/blob/master/README.md#running-tests
 set -e
